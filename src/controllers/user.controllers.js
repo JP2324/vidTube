81709dd5da089a0018aa39dce8752ea658359f25
Jainam Patel <jainampatel2409@gmail.com>
2025-09-29 21:18:03 +0530

    LogIn user controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,6 +4,26 @@ import {User} from "../models/user.models.js"
 import { uploadOnCloudinary , deleteFromCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
+// Generate access token and refresh token
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        if(!user){
+            throw new ApiError(404,"User not found")
+        }
+        const accessToken  = user.generateAccessToken() // generate new refresh token
+        const refreshToken  = user.generateRefreshToken() // generate new refresh token
+    
+        user.refreshToken = refreshToken // save new refresh token in db
+        await user.save({validateBeforeSave:false})
+        
+        return {accessToken,refreshToken}
+    } catch (error) {
+        throw new ApiError(500,"Something went wrong while generating tokens")
+    }
+}
+
+// Register user controller
 const registerUser = asyncHandler(async (req, res) => {
     // Registration logic here
     const  {fullName,email,username,password} = req.body
@@ -87,4 +107,44 @@ const registerUser = asyncHandler(async (req, res) => {
 
 })
 
-export { registerUser }
+// Login user controller
+const loginUser = asyncHandler(async (req, res) => {
+    // get data from request body 
+    const {email,username,password} = req.body
+    //validation
+    if(!email){
+        throw new ApiError(400,"Email is required")
+    }
+    if(!password){
+        throw new ApiError(400,"Password is required")
+    }
+    const user = await User.findOne({
+        $or: [{username},{email}]
+    })
+    if(!user){
+        throw new ApiError(404,"User not found")
+    }
+    //validate password
+    const isPasswordValid=await user.isPasswordCorrect(password)
+    if(!isPasswordValid){
+        throw new ApiError(401,"Invalid credentials")
+    }
+    const {accessToken,refreshToken} = await generateAccessAndRefreshToken(user._id)
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    
+    const options ={
+        httpOnly:true,
+        secure : process.env.NODE_ENV === "production",
+    }
+    return res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(new ApiResponse(200,{user:loggedInUser,accessToken,refreshToken},"User logged in successfully"))
+
+})
+
+
+
+export { registerUser , loginUser }
